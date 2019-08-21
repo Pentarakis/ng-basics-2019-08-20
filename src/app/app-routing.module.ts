9ca6a1925f4efa66502bf85edac1686050468ed9
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-08-21 15:09:07 +0200

    CharacterComponentList - Zwischenstand

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './basic-layout/dashboard/dashboard.component';
+import { CharacterListComponent } from './character/character-list/character-list.component';
 import { CharacterComponent } from './character/character/character.component';
 
 const routes: Routes = [
   { path : '', component: DashboardComponent},
-  { path : 'character', component: CharacterComponent}
+  { path : 'character', component: CharacterListComponent}
 ];
 
 @NgModule({
--- src/app/character/character.module.ts
+++ src/app/character/character.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { CharacterRoutingModule } from './character-routing.module';
 import { CharacterComponent } from './character/character.component';
+import { CharacterListComponent } from './character-list/character-list.component';
 
 
 @NgModule({
-  declarations: [CharacterComponent],
-  exports: [CharacterComponent],
+  declarations: [CharacterComponent, CharacterListComponent],
+  exports: [CharacterComponent, CharacterListComponent],
   imports: [
     CommonModule,
     CharacterRoutingModule,

fd458e3aa3df734c897ce5e86353ca9e64fc6fb4
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-08-22 13:56:33 +0200

    Fehlerbehandlung mit catchError

--- src/app/character/character.service.ts
+++ src/app/character/character.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Character } from './model/character';
 
 @Injectable({
@@ -18,19 +19,48 @@ export class CharacterService {
   constructor(private httpClient: HttpClient) { }
 
   readAll(): Observable<Character[]> {
-    return this.httpClient.get<Character[]>(`${this.baseUrl}?_start=20&_end=120`);
+    return this.httpClient
+      .get<Character[]>(`${this.baseUrl}?_start=20&_end=120`)
+      .pipe(
+        catchError(() => {
+          this.handleError('Fehler beim Laden der Characters!');
+          return of([]);
+        })
+      );
   }
 
   read(id: number): Observable<Character> {
-    return this.httpClient.get<Character>(`${this.baseUrl}/${id}`);
+    return this.httpClient.get<Character>(`${this.baseUrl}/${id}`)
+      .pipe(
+        catchError(() => {
+          this.handleError(`Fehler beim Laden des Characters mit ${id}!`);
+          return of(null);
+        })
+      );
   }
 
   create(character: Character): Observable<Character> {
-    return this.httpClient.post<Character>(this.baseUrl, character);
+    return this.httpClient.post<Character>(this.baseUrl, character)
+      .pipe(
+        catchError(() => {
+          this.handleError(`Fehler beim Anlegen des neuen Characters mit!`);
+          return of(null);
+        })
+      );
   }
 
   update(character: Character): Observable<Character> {
     return this.httpClient
-      .put<Character>(`${this.baseUrl}/${character.id}`, character);
+      .put<Character>(`${this.baseUrl}/${character.id}`, character)
+      .pipe(
+        catchError(() => {
+          this.handleError(`Fehler beim Speichern des Characters mit ${character.id}!`);
+          return of(null);
+        })
+      );
+  }
+
+  private handleError(msg: string): void {
+    alert(msg);
   }
 }

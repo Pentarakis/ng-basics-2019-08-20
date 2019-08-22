3aab0cbf5e224c02a52eaefe15749a73900d0c37
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-08-22 14:25:28 +0200

    Params-Observable und takeUntil

--- src/app/character/character/character.component.ts
+++ src/app/character/character/character.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subject, Subscription } from 'rxjs';
 import { filter } from 'rxjs/internal/operators/filter';
+import { catchError, pluck, switchMap, takeUntil } from 'rxjs/operators';
 import { CharacterService } from '../character.service';
 import { Character } from '../model/character';
 
@@ -9,10 +11,11 @@ import { Character } from '../model/character';
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss']
 })
-export class CharacterComponent implements OnInit {
+export class CharacterComponent implements OnInit, OnDestroy {
 
   character: Character = new Character();
-  isCreateMode = false;
+  isCreateMode = true;
+  private destroy = new Subject<boolean>();
 
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -20,24 +23,27 @@ export class CharacterComponent implements OnInit {
   }
 
   ngOnInit() {
-    const id: string = this.route.snapshot.params.id;
-
-    if (id === 'create') {
-      this.isCreateMode = true;
-    } else {
-
-      this.characterService.read(Number(id))
-        .subscribe(
-          (character: Character) => this.character = character
-        );
-    }
+    this.route.params
+      .pipe(
+        pluck('id'),
+        filter(id => id !== 'create'),
+        switchMap((id: number) => this.characterService.read(Number(id))),
+        takeUntil(this.destroy)
+      )
+      .subscribe(
+        (character: Character) => {
+          this.isCreateMode = false;
+          this.character = character;
+        }
+      );
   }
 
   save(): void {
     if (this.isCreateMode) {
       this.characterService.create(this.character)
         .pipe(
-          filter(data => data !== null)
+          filter(data => data !== null),
+          takeUntil(this.destroy)
         )
         .subscribe(
           (character: Character) => alert('Success! ID: ' + character.id)
@@ -45,13 +51,19 @@ export class CharacterComponent implements OnInit {
     } else {
       this.characterService.update(this.character)
         .pipe(
-          filter(data => data !== null)
+          filter(data => data !== null),
+          takeUntil(this.destroy)
         )
         .subscribe(() => alert('success'));
     }
     this.router.navigate(['../'], {
       relativeTo: this.route
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.complete();
   }
 
 }
